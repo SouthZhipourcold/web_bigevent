bfbe06fa0c58755a4b2247e0746a02a6906b5cdb
武博 <1509813654@qq.com>
2021-02-23 18:49:54 +0800

    点击退出

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,5 +1,12 @@
 $.ajaxPrefilter(function(options) {
     // 发送Ajxs请求前,统一拼接请求的根路径
     options.url = 'http://ajax.frontend.itheima.net' + options.url;
-    console.log(options.url);
+    // console.log(options.url);
+
+    // 统一为有权限的接口,设置headers请求
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.token || '',
+        };
+    }
 })
